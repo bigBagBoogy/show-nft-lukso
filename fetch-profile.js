9da4b3e3ec44732bc48726d50eba914250efa9f7
maarten <van_den_bergmaarten@hotmail.com>
2023-09-15 20:24:47 +0200

    amended code

---
+++ fetch-profile.js
@@ -0,0 +1,36 @@
+// Imports
+const { Web3 } = require("web3");
+const { ERC725 } = require("@erc725/erc725.js");
+require("isomorphic-fetch");
+
+// Static variables
+const RPC_ENDPOINT = "https://rpc.l16.lukso.network";
+const IPFS_GATEWAY = "https://cloudflare-ipfs.com/ipfs/";
+const SAMPLE_PROFILE_ADDRESS = "0xa907c1904c22DFd37FF56c1f3c3d795682539196";
+
+// Parameters for ERC725 Instance
+const erc725schema = require("@erc725/erc725.js/schemas/LSP3UniversalProfileMetadata.json");
+const provider = new Web3.providers.HttpProvider(process.env.LUKSO_RPC_URL);
+
+const config = { ipfsGateway: IPFS_GATEWAY };
+
+/*
+ * Fetch the @param's Universal Profile's
+ * LSP3 data
+ *
+ * @param address of Universal Profile
+ * @return string JSON or custom error
+ */
+async function fetchProfileData(address) {
+  try {
+    const profile = new ERC725(erc725schema, address, provider, config);
+    return await profile.fetchData();
+  } catch (error) {
+    return console.log("This is not an ERC725 Contract: ", error);
+  }
+}
+
+// Debug
+fetchProfileData(SAMPLE_PROFILE_ADDRESS).then((profileData) =>
+  console.log(JSON.stringify(profileData, undefined, 2))
+);
