c12aabd5b5f96a3f670082b6f51d01a33751f600
maarten <van_den_bergmaarten@hotmail.com>
2023-09-16 09:46:47 +0200

    amended code

--- fetch-profile.js
+++ fetch-profile.js
@@ -4,13 +4,16 @@ const { ERC725 } = require("@erc725/erc725.js");
 require("isomorphic-fetch");
 
 // Static variables
-const RPC_ENDPOINT = "https://rpc.l16.lukso.network";
-const IPFS_GATEWAY = "https://cloudflare-ipfs.com/ipfs/";
-const SAMPLE_PROFILE_ADDRESS = "0xa907c1904c22DFd37FF56c1f3c3d795682539196";
+// const RPC_ENDPOINT = process.env.GATEWAY_FM_RPC_URL;
+
+const SAMPLE_PROFILE_ADDRESS = "0x3F0350EaFc25Cc9185a77394B7E2440ec002e466";
+const RPC_ENDPOINT = "https://rpc.testnet.lukso.network";
+const IPFS_GATEWAY = "https://2eff.lukso.dev/ipfs/";
 
 // Parameters for ERC725 Instance
 const erc725schema = require("@erc725/erc725.js/schemas/LSP3UniversalProfileMetadata.json");
-const provider = new Web3.providers.HttpProvider(process.env.LUKSO_RPC_URL);
+const provider = new Web3.providers.HttpProvider(RPC_ENDPOINT);
+//process.env.LUKSO_RPC_URL
 
 const config = { ipfsGateway: IPFS_GATEWAY };
 
--- index.js
+++ index.js
@@ -5,14 +5,46 @@ const { Web3 } = require("web3");
 const web3 = new Web3(
   new Web3.providers.HttpProvider(process.env.LUKSO_RPC_URL)
 );
-// const web3 = new Web3(process.env.LUKSO_RPC_URL);
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
-});
 const port = 3000; // You can choose any available port
+
+// Define the route that connects to the UP Browser Extension
+app.get("/connect", async (req, res) => {
+  try {
+    // Check if the UP Extension is available
+    if (window.ethereum) {
+      // Initialize a Web3 instance with the UP Extension as the provider
+      const web3 = new Web3(window.ethereum);
+
+      // Request access to accounts
+      const accountsRequest = await web3.eth.requestAccounts();
+      const accounts = await web3.eth.getAccounts();
+
+      // Return the connected address as a response
+      res.json({ address: accounts[0] });
+    } else {
+      res.status(400).json({ error: "UP Extension is not installed." });
+    }
+  } catch (error) {
+    console.error("Error connecting to UP Extension:", error);
+    res.status(500).json({ error: "Error connecting to UP Extension" });
+  }
+});
+
+// Start the Express.js server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-// Export the web3 instance to make it accessible in other files
-module.exports = web3;
+
+// // const web3 = new Web3(process.env.LUKSO_RPC_URL);
+// const app = express();
+// app.get("/", (req, res) => {
+//   res.send("Hello, Express!");
+// });
+// const port = 3000; // You can choose any available port
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+// // Export the web3 instance to make it accessible in other files
+// module.exports = web3;
